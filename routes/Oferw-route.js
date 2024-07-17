52cb74c7327d7f0a6fc68eea4caffc01465fd6a2
oferw5 <161222266+oferw5@users.noreply.github.com>
2024-07-17 21:23:27 +0300

    Ofer-route

---
+++ routes/Oferw-route.js
@@ -0,0 +1,5 @@
+module.exports = (app) => {
+  app.get("/Oferw", (req, res) => {
+    res.send("This is an Oferw route!");
+  });
+};
--- routes/ofer-route.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) => {
-  app.get("/ofer", (req, res) => {
-    res.send("This is an ofer route!");
-  });
-};
