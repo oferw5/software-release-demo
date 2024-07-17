d2801cabab8bf5d38a6a15ea6ffcd8babd27f8f7
YoavKatzz <165892808+YoavKatzz@users.noreply.github.com>
2024-07-17 21:07:10 +0300

    Fixed Yoav route

--- routes/yoav-route.js
+++ routes/yoav-route.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
   app.get("/yoav", (req, res) => {
-    res.send("Yoav was here, he created this icredible route and brought it before us");
+    res.send("Yoav was here, he created this incredible route and brought it before us");
   });
 };
